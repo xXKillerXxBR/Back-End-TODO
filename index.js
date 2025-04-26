b08b177a302237e2b1d8fd460bddaffb77aa8eb1
Thiago Lira <thiagolira657@gmail.com>
2025-04-25 22:57:30 -0300

    Arrumando 502 p1

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.listen(PORT, () => {
 })
 // Obtendo os parametros passados pela linha de comando
 var userArgs = process.argv.slice(2);
-var mongoURL = userArgs[0];
+const mongoURL = process.env.MONGODB_URI;
+
 //Configurando a conexao com o Banco de Dados
 var mongoose = require('mongoose');
 mongoose.connect(mongoURL);
